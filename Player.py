7e34f9c0cddda5be793481da1e19899bf42a50f5
Gavin Burns <burnsg@wit.edu>
2024-02-01 10:17:52 -0500

    more implementations
    -added roomcheck to players movement
    -disabled description of rooms are dark w no torch
    -implemented hasKey in player()

--- Player.py
+++ Player.py
@@ -1,3 +1,4 @@
+import room.Py
 class Player:
     def __init__(self, name, starting_room):
         self.name = name
@@ -9,7 +10,8 @@ class Player:
     def display_status(self):
         print(f"{self.name}'s Status:")
         print(f"Current Room: {self.current_room.name}")
-        print(f"Description: {self.current_room.description}")
+        if ((not self.current_room.isDark) and ('torch' not in self.inventory)):
+            print(f"Description: {self.current_room.description}")
         print(f"Health: {self.health}")
         print(f"Inventory: {self.inventory}")
 
@@ -17,17 +19,25 @@ class Player:
         if direction.lower() == 'north' and self.current_room.north:
             self.current_room = self.current_room.north
             print(f"{self.name} moved to the north.")
+            self.current_room.roomCheck(self.current_room, self)
         elif direction.lower() == 'south' and self.current_room.south:
             self.current_room = self.current_room.south
             print(f"{self.name} moved to the south.")
+            self.current_room.roomCheck(self.current_room, self)
         elif direction.lower() == 'east' and self.current_room.east:
             self.current_room = self.current_room.east
             print(f"{self.name} moved to the east.")
+            self.current_room.roomCheck(self.current_room, self)
         elif direction.lower() == 'west' and self.current_room.west:
             self.current_room = self.current_room.west
             print(f"{self.name} moved to the west.")
+            self.current_room.roomCheck(self.current_room, self)
         else:
             print(f"There is no path to the {direction}.")
+    
+    def hasKey(self):
+        if ('key' in self.inventory):
+            return True
 
     def pick_up_item(self, item):
         if len(self.inventory) < self.max_inventory_slots:
