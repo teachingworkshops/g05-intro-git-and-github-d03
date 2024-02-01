7c548adcd6e5d185e335447bb06a8667b0b2ec7a
dextern1atwit <90652288+dextern1atwit@users.noreply.github.com>
2024-01-31 22:54:35 -0500

    Description strings in main

---
+++ main.py
@@ -0,0 +1,32 @@
+#Room Descriptions
+start_desc = "Damp stone brick walls surround you. Light enters from the gaping hole in the ceiling from which you first fell in. There are doorways splitting to the east, west and south. There must be another way out..."
+hall_desc = "Ahead is straight corridor with doorways only to the north and south."
+deadend_desc = "You find yourself in an empty dead-end room. Better turn around.."
+east_north_turn = "The stone walls curve at a right angle, a doorway to the north and tp the east."
+east_south_turn = "The stone walls curve at a right angle, a doorway to the south and to the east."
+west_north_turn = "The stone walls curve at a right angle, a doorway to the north and to the west."
+north_south_west = "The path splits off into 3 directions, to the north south or west."
+north_south_east = "The path splits off into 3 directions, to the north south or east."
+
+#Item Descriptions
+key_desc = "A tarnished brass skeleton key, no doubt for opening a door somewhere.."
+discover_key = "A tiny yellow shimmer catches your eye on the floor. You find a brass key and put in it your pocket."
+discover_sword = "A rotten goblin lay slayn on the ground with a longsword sticking out from its chest. You remove the blade and carry it with you."
+sword_desc = "A shimering steel sword with a golden hilt, it feels heavy in your hands. Must have belonged to some other adventurer. "
+discover_shield = "You see a charred human skeleton laying in a pile of soot, a silver shield covering its chest. You pick up the shield in your offhand to take with you, maybe it will serve you beter."
+shield_desc = "A shiny silver kite shield strapped to your arm. Don't let those monsters get too close.."
+discover_potion = "In a small wooden box you find a vial of red liquid. Just sniffing the concoction makes you buzz with energy. You collect the potion as it may come in handy."
+potion_desc = "A rose red potion of what you can only asume gives you more vitality."
+discover_torch = "Mounted on the wall is a lit torch illuminating the room. You pick it up off the wall in case you need to explore any dark depths ahead."
+torch_desc = "A seeminly normal lit wooden torch, besides the fact that no matter what you do nothing seems to extingish the flames. Strange.."
+
+#Enemy Descriptions
+goblin_desc = "A hunched green creature with pointed ears and a rusty dagger ambushes you! Classic Goblin tactics."
+goblin_hit = "The goblin swings its dagger at you and makes contact, spilling your blood on the dungeon floor."
+goblin_defeat = "You fell the lowly goblin with your attacks, putting it out of its misery."
+
+minotaur_desc = "A tall, half man half bull creature holding a hulking axe blocks your path. The Minotaur lets out a bellow and rushes towards you."
+minotaur_hit = "You are unable to avoid the Minotaurs swing, giving you a large gash from the axe blade."
+minotaur_defeat = "The minotaur bellows once more and falls to the ground after you deliver a mighty final blow to the beast."
+
+defeat_msg = "You fall to the gorund, bleeding out from your injuries. You simply cannot go on...    GAME OVER!"
