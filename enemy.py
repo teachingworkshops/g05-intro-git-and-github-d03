8c5e67e02961d7bb0161201871db055fd213d597
bisessarnATWIT <114747550+bisessarnATWIT@users.noreply.github.com>
2024-01-31 21:20:56 -0500

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,14 +1,4 @@
 class Enemy:
-    def __init__(self, name, description, health, attack_bonus):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.health = health
-        self.attack_bonus = attack_bonus
-
-#The is_alive method checks if the enemy is still alive
-    def is_alive(self):
-        return self.health > 0
-    
-#The take_damage method reduces the enemy's health.
-    def take_damage(self, damage):
-        self.health -= damage
