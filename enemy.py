2124df5ab2b0392ade69031dcdba55b171b1dac5
Cameron Dugan <cameron.dugan@protonmail.com>
2024-02-04 12:11:38 -0500

    made main.py runnable, fixed some method calls

--- enemy.py
+++ enemy.py
@@ -2,36 +2,33 @@ import random
 
 
 class Enemy:
+    def __init__(self, name, description, hit, defeat):
+        self.name = name
+        self.description = description
+        self.defeat = defeat
+        self.hit = hit
 
-  def __init__(self, name, description, hit, defeat):
-    self.name = name
-    self.description = description
-    self.defeat = defeat
-    self.hit = hit
+    def attack(self, player):
+        print(self.description)
+        while player.health > 0:
+            player_roll = random.randint(1, 6)
+            enemy_roll = random.randint(1, 6)
 
-  import random
+            player_attack_bonus = 2 if player.hasSword else 0
+            enemy_attack_bonus = -2 if player.hasShield else 0
 
-  def attack(self, player):
-    print(self.description)
-    while player.health > 0:
-      player_roll = random.randint(1, 6)
-      enemy_roll = random.randint(1, 6)
+            player_total = player_roll + player_attack_bonus
+            enemy_total = enemy_roll + enemy_attack_bonus
 
-      player_attack_bonus = 2 if player.hasSword else 0
-      enemy_attack_bonus = -2 if player.hasShield else 0
+            if player_total > enemy_total:
+                print(self.defeat)
+                break
+            elif player_total < enemy_total:
+                print(self.hit)
+                player.health -= 1
 
-      player_total = player_roll + player_attack_bonus
-      enemy_total = enemy_roll + enemy_attack_bonus
-
-      if player_total > enemy_total:
-        print(self.defeat)
-        break
-      elif player_total < enemy_total:
-        print(self.hit)
-        player.health -= 1
-
-    if player.health <= 0:
-      print(
-          "You fall to the gorund, bleeding out from your injuries. You simply cannot go on...    GAME OVER!"
-      )
-      exit()
+        if player.health <= 0:
+            print(
+                "You fall to the gorund, bleeding out from your injuries. You simply cannot go on...    GAME OVER!"
+            )
+            exit()
--- item(1).py
+++ item(1).py
@@ -1,9 +1,8 @@
 class Item:
-
-  def __init__(self, name, description, discovery):
-    #title of item appearing in inventory
-    self.name = name
-    #text displayed when first picking up the item (automatic action)
-    self.discovery = discovery
-    #text displayed when item is inspected in inventory
-    self.description = description
+    def __init__(self, name, description, discovery):
+        # title of item appearing in inventory
+        self.name = name
+        # text displayed when first picking up the item (automatic action)
+        self.discovery = discovery
+        # text displayed when item is inspected in inventory
+        self.description = description
